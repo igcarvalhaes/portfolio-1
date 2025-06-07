936f5ec6786ecbe76e55ba402896de05d1034ade
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-06-07 17:32:37 -0300

    Feat: Adicionei main e section. Texto com info do dev e foto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { Github, Linkedin, Mail, Twitter } from "lucide-react";
 
 function App() {
   return (
@@ -9,22 +10,63 @@ function App() {
         <main>
           <section
             id="about"
-            className="min-h-screen flex items-center justify-center"
+            className="min-h-screen flex items-center justify-center flex-col"
           >
-            <div>
-              <h2>Igor Carvalhaes</h2>
-              <p>
-                Sou um desenvolvedor front-end, estudante de Engenharia de
-                Computação na UERJ, apaixonado por criar interfaces bonitas e
-                interativas, focadas em usabilidades, desempenho e
-                acessibilidade, utilizando boas práticas de código limpo.
-              </p>
-              <nav>
-                <a href="#">Email</a>
-                <a href="#">Twitter</a>
-                <a href="#">Github</a>
-                <a href="#">Linkedin</a>
-              </nav>
+            <div className="flex flex-col items-center gap-12 md:gap-32 md:flex-row md:justify-center md:items-center w-full md:max-w-5xl">
+              {/* Foto */}
+              <div className="order-1 md:order-2 w-full  flex justify-center">
+                <div className="w-64 h-64 md:w-110 md:h-110">
+                  <img
+                    src="https://github.com/igcarvalhaes.png"
+                    alt="Foto de perfil"
+                    className="object-cover rounded-full w-full h-full"
+                  />
+                </div>
+              </div>
+
+              {/* Texto */}
+              <div className="order-2 gap-5 md:gap-15 md:order-1 w-full flex flex-col items-center md:items-start justify-center text-center md:text-left px-4">
+                <h2 className="text-white text-3xl font-bold md:text-6xl md:text-left">
+                  Igor Carvalhaes
+                </h2>
+                <p className="text-lg md:text-3xl font-light mx-2 md:w-180 md:text-left md:mx-0">
+                  Sou um desenvolvedor front-end, estudante de Engenharia de
+                  Computação na UERJ, apaixonado por criar interfaces bonitas e
+                  interativas, focadas em usabilidades, desempenho e
+                  acessibilidade, utilizando boas práticas de código limpo.
+                </p>
+                <nav
+                  className="flex w-full justify-center gap-10 md:justify-start"
+                  aria-label="links para redes sociais"
+                >
+                  <a
+                    href="mailto:igorcarvalhaes123@gmail.com"
+                    target="_blank"
+                    title="Entre em contato por email"
+                    className="link-underline flex items-center gap-2 p-2 rounded-lg transition-all duration-200 text-neutral-300 hover:text-neutral-50"
+                  >
+                    <Mail size={30} />
+                  </a>
+                  <a
+                    href="https://x.com/igcarvalhaes"
+                    className="link-underline flex items-center gap-2 p-2 rounded-lg transition-all duration-200 text-neutral-300 hover:text-neutral-50"
+                  >
+                    <Twitter size={30} />
+                  </a>
+                  <a
+                    href="https://github.com/igcarvalhaes/"
+                    className="link-underline flex items-center gap-2 p-2 rounded-lg transition-all duration-200 text-neutral-300 hover:text-neutral-50"
+                  >
+                    <Github size={30} />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/igor-carvalhaes/"
+                    className="link-underline flex items-center gap-2 p-2 rounded-lg transition-all duration-200 text-neutral-300 hover:text-neutral-50"
+                  >
+                    <Linkedin size={30} />
+                  </a>
+                </nav>
+              </div>
             </div>
           </section>
         </main>

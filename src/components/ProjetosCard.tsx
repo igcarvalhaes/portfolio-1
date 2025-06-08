b444cb24b609072cfafee3c2caca04b63a300ba4
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-06-07 21:08:59 -0300

    Style: aumentei tamanho da fonte do card em telas maiores

--- src/components/ProjetosCard.tsx
+++ src/components/ProjetosCard.tsx
@@ -17,7 +17,7 @@ export function ProjetosCard({
     <article className="p-6 rounded-lg border-1 border-neutral-700 bg-neutral-800 max-w-2xl  md:max-w-7xl">
       <div className="flex flex-col md:p-12">
         <div className="flex justify-between mb-3 pb-4 ">
-          <span className="font-bold text-2xl md:text-3xl">
+          <span className="font-bold text-2xl  md:text-3xl">
             <a
               className="relative hover:text-neutral-100 after:content-[''] after:bg-neutral-300 after:h-[3px] after:w-0 after:left-0 after:-bottom-[5px] after:rounded-xl after:absolute after:transition-width after:transition-all after:duration-500 hover:after:w-full"
               target="_blank"
@@ -28,7 +28,7 @@ export function ProjetosCard({
           </span>
         </div>
         <div className="pt-4 mb-4">
-          <p className="text-1xl md:text-2xl text-left">{descricao}</p>
+          <p className="text-xl md:text-2xl text-left">{descricao}</p>
         </div>
         <div className="pt-4 mt-4 border-t border-neutral-700">
           <a

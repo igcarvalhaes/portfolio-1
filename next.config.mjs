0db7a61fd72a2d0ea9b1a2d01bd7eaabd0394e39
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-12-06 00:23:25 -0300

    Configurar imagens externas do GitHub e adicionar placeholders para logos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
